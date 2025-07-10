0e21be80bdb560a2653f44efbafa422ce68ea714
Mavodeli <contact@mavodeli.de>
2025-07-10 05:57:08 +0200

    add icon and README to zipper

--- zipper.bat
+++ zipper.bat
@@ -1,6 +1,9 @@
 cd build
 DEL "risk_of_multiprinting.zip"
 cd ..
+xcopy "manifest.json" "%CD%\risk_of_multiprinting\bin\Debug\netstandard2.1"
+xcopy "icon.png" "%CD%\risk_of_multiprinting\bin\Debug\netstandard2.1"
+xcopy "README.md" "%CD%\risk_of_multiprinting\bin\Debug\netstandard2.1"
 7z a -y -tzip "./build/risk_of_multiprinting.zip" "./risk_of_multiprinting/bin/Debug/netstandard2.1/*" -mx9
 echo done
 exit
