da2d0c37e3a15f4f268fad58df57d250191d6c29
Mavodeli <contact@mavodeli.de>
2025-07-10 07:13:16 +0200

    fix icon and zipper updating

--- zipper.bat
+++ zipper.bat
@@ -1,10 +1,10 @@
 cd build
 DEL "risk_of_multiprinting.zip"
 cd ..
-xcopy "manifest.json" "%CD%\risk_of_multiprinting\bin\Debug\netstandard2.1"
-xcopy "icon.png" "%CD%\risk_of_multiprinting\bin\Debug\netstandard2.1"
-xcopy "README.md" "%CD%\risk_of_multiprinting\bin\Debug\netstandard2.1"
-xcopy "CHANGELOG.md" "%CD%\risk_of_multiprinting\bin\Debug\netstandard2.1"
+7z a -y -tzip "./build/risk_of_multiprinting.zip" "manifest.json"
+7z a -y -tzip "./build/risk_of_multiprinting.zip" "icon.png"
+7z a -y -tzip "./build/risk_of_multiprinting.zip" "README.md"
+7z a -y -tzip "./build/risk_of_multiprinting.zip" "CHANGELOG.md"
 7z a -y -tzip "./build/risk_of_multiprinting.zip" "./risk_of_multiprinting/bin/Debug/netstandard2.1/*" -mx9
 echo done
 exit
